dc6aa2138fe3031bedad8778a7779be887dfcb6e
jeffinjk <jeffinkurian6@gmail.com>
2024-08-10 00:06:33 +0530

    jhb

--- sign-in/time.js
+++ sign-in/time.js
@@ -1,20 +1,14 @@
 function time() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    const currentTime = `${hours}:${minutes}:${seconds}`;
-    document.getElementById('timeButton').innerText = currentTime;
+  const now = new Date();
+  let hours = now.getHours().toString().padStart(2, '0');
+  let minutes = now.getMinutes().toString().padStart(2, '0');
+  let seconds = now.getSeconds().toString().padStart(2, '0');
+  const currentTime = `${hours}:${minutes}:${seconds}`;
+  document.getElementById('timeButton').innerText = currentTime;
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    time();
-    function updateTime() {
-        time();
-        requestAnimationFrame(updateTime);
-      }
-      
-      document.addEventListener('DOMContentLoaded', () => {
-        updateTime();
-      });
+  time();
+  setInterval(time, 1000); 
 });
+
